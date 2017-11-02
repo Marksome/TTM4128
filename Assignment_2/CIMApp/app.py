2c527cf97db6a30fbebc6e64ae08498935623996
Markus Andersen <markus.s.andersen@gmail.com>
2017-11-02 18:47:16 +0100

    Added snmp to the app.

--- Assignment_2/CIMApp/app.py
+++ Assignment_2/CIMApp/app.py
@@ -1,24 +1,22 @@
 from flask import Flask, render_template
-import pywbem, pysnmp
+import pywbem
+from subprocess import check_output as out
 
 app = Flask(__name__)
 wbem = pywbem.WBEMConnection('http://ttm4128.item.ntnu.no:5988/cimom')
 
-@app.route('/OS')
-def os():
-    os = wbem.EnumerateInstances('CIM_OperatingSystem')[0]['Version']#.replace(' ', '\n')
-    return render_template('index.html', os=os)
-
 @app.route('/CIM')
 def cim():
-    cim = ''
+    os = wbem.EnumerateInstances('CIM_OperatingSystem')[0]['Version']
+    interface = ''
     interfaces = wbem.EnumerateInstances('CIM_IPProtocolEndpoint')
     for i in interfaces:
-        cim += 'Name=' + i['Name'] + '\nIPv4Address=' + i['IPv4Address'] + '\nSubnetMask=' + i['SubnetMask'] + '\n\n'
-    return render_template('index.html', cim=cim)
+        interface += 'Name=' + i['Name'] + '\nIPv4Address=' + i['IPv4Address'] + '\nSubnetMask=' + i['SubnetMask'] + '\n\n'
+    return render_template('index.html', os=os, interface=interface)
 
 @app.route('/SNMP')
 def snmp():
+    snmp = out('snmpwalk -v 2c -c ttm4128 129.241.200.173 1.3.6.1.2.1.1.1'.split())
     return render_template('index.html', snmp=snmp)
 
 @app.route("/")
