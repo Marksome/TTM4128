2d2158c26f132a48c14aedbde421d0d0588955f3
Markus Andersen <markus.s.andersen@gmail.com>
2017-11-02 15:53:25 +0100

    Changed variable names in app.py

--- Assignment_2/CIMApp/app.py
+++ Assignment_2/CIMApp/app.py
@@ -2,24 +2,23 @@ from flask import Flask, render_template
 import pywbem, pysnmp
 
 app = Flask(__name__)
-conn = pywbem.WBEMConnection('http://ttm4128.item.ntnu.no:5988/cimom')
+wbem = pywbem.WBEMConnection('http://ttm4128.item.ntnu.no:5988/cimom')
 
 @app.route('/OS')
 def os():
-    os = conn.EnumerateInstances('CIM_OperatingSystem')[0]['Version']#.replace(' ', '\n')
+    os = wbem.EnumerateInstances('CIM_OperatingSystem')[0]['Version']#.replace(' ', '\n')
     return render_template('index.html', os=os)
 
 @app.route('/CIM')
 def cim():
     cim = ''
-    interfaces = conn.EnumerateInstances('CIM_IPProtocolEndpoint')
+    interfaces = wbem.EnumerateInstances('CIM_IPProtocolEndpoint')
     for i in interfaces:
         cim += 'Name=' + i['Name'] + '\nIPv4Address=' + i['IPv4Address'] + '\nSubnetMask=' + i['SubnetMask'] + '\n\n'
     return render_template('index.html', cim=cim)
 
 @app.route('/SNMP')
 def snmp():
-
     return render_template('index.html', snmp=snmp)
 
 @app.route("/")
