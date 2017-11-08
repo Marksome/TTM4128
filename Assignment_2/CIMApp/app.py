d4e653ef25b5b34514fbcbae8a858335fbcb3736
Markus Andersen <markus.s.andersen@gmail.com>
2017-11-08 15:01:52 +0100

    Changed the queries and added snmp compatability

--- Assignment_2/CIMApp/app.py
+++ Assignment_2/CIMApp/app.py
@@ -1,23 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 import pywbem
 from subprocess import check_output as out
 
 app = Flask(__name__)
-wbem = pywbem.WBEMConnection('http://ttm4128.item.ntnu.no:5988/cimom')
 
-@app.route('/CIM')
+@app.route('/CIM', methods=['POST'])
 def cim():
-    os = wbem.EnumerateInstances('CIM_OperatingSystem')[0]['Version']
+    name = request.form['name']
+    wbem = pywbem.WBEMConnection(name)
+    os = wbem.EnumerateInstances('CIM_OperatingSystem')[0]['Version'].split()
+    os = os[0] + '\n' + os[1]
     interface = ''
     interfaces = wbem.EnumerateInstances('CIM_IPProtocolEndpoint')
     for i in interfaces:
-        interface += 'Name=' + i['Name'] + '\nIPv4Address=' + i['IPv4Address'] + '\nSubnetMask=' + i['SubnetMask'] + '\n\n'
+        interface += 'Name=' + i['Name'] + '\nIPv4Address=' + i['IPv4Address'] + '\n\n'
     return render_template('index.html', os=os, interface=interface)
 
-@app.route('/SNMP')
+@app.route('/SNMP', methods=['POST'])
 def snmp():
-    snmp = out('snmpwalk -v 2c -c ttm4128 129.241.200.173 1.3.6.1.2.1.1.1'.split())
-    return render_template('index.html', snmp=snmp)
+    name = request.form['name']
+    query = 'snmpwalk -v 2c -c ttm4128 ' + name + ' 1.3.6.1.2.1.1.1'
+    try:
+        snmp = out(query.split()).split('"')[1]
+    except:
+        snmp = 'Error: SNMP Service not running on ' + name
+    query = 'snmpwalk -v 2c -c ttm4128 ' + name + ' 1.3.6.1.2.1.4.20.1.1'
+    try:
+        snmp_interface = 'IPv4Address=' + out(query.split()).split()[7]
+    except:
+        snmp_interface = ''
+    return render_template('index.html', snmp=snmp, snmp_interface=snmp_interface)
 
 @app.route("/")
 def main():
